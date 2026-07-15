d15d8a97fd66c85a135ea439dd6c11c0f4877a05
andr3vdb <andre@acw.co.za>
2026-07-15 09:47:55 +0200

    update

--- install.sql
+++ install.sql
@@ -1,60 +1,37 @@
-CREATE TABLE IF NOT EXISTS acw_supplier_profiles
-(
-    supplier_id INT AUTO_INCREMENT PRIMARY KEY,
+DROP TABLE IF EXISTS `ac_banners`;
+CREATE TABLE `ac_banners` (
+	`banner_id` int(11) NOT NULL AUTO_INCREMENT,
+	`status` int(1) NOT NULL DEFAULT '0',
+	`banner_type` int(11) NOT NULL DEFAULT '1',
+	`banner_group_name` varchar(255) NOT NULL DEFAULT '',
+	`start_date` timestamp NULL DEFAULT NULL,
+	`end_date` timestamp NULL DEFAULT NULL,
+	`blank` tinyint(1) NOT NULL DEFAULT '0',
+	`target_url` text DEFAULT '',
+	`sort_order` int(11) NOT NULL,
+	`date_added` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`banner_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-    supplier_name VARCHAR(100),
+DROP TABLE IF EXISTS `ac_banner_descriptions`;
+CREATE TABLE `ac_banner_descriptions` (
+  `banner_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `meta` text(1500) DEFAULT '',
+	`date_added` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`date_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`banner_id`,`language_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-    file_type VARCHAR(10),
-
-    sku_column VARCHAR(50),
-
-    description_column VARCHAR(50),
-
-    cost_column VARCHAR(50),
-
-    stock_column VARCHAR(50),
-
-    brand_column VARCHAR(50),
-
-    category_column VARCHAR(50),
-
-    active TINYINT DEFAULT 1
-);
-
-CREATE TABLE IF NOT EXISTS acw_import_stage
-(
-    stage_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-
-    supplier VARCHAR(100),
-
-    supplier_sku VARCHAR(100),
-
-    product_name VARCHAR(255),
-
-    brand VARCHAR(100),
-
-    category VARCHAR(100),
-
-    cost DECIMAL(15,2),
-
-    stock INT,
-
-    imported_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS acw_import_history
-(
-    import_id INT AUTO_INCREMENT PRIMARY KEY,
-
-    supplier VARCHAR(100),
-
-    filename VARCHAR(255),
-
-    products INT,
-
-    updated INT,
-
-    skipped INT,
-
-    imported_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+DROP TABLE IF EXISTS `ac_banner_stat`;
+CREATE TABLE `ac_banner_stat` (
+  `banner_id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `store_id` int(11) NOT NULL,
+  `user_info` text(1500) DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE INDEX `banner_stat_idx` ON `ac_banner_stat` (`banner_id`, `type`, `time`, `store_id`);
--- main.php
+++ main.php
@@ -1,24 +1,69 @@
 <?php
-
+/*
+ *   $Id$
+ *
+ *   AbanteCart, Ideal OpenSource Ecommerce Solution
+ *   http://www.AbanteCart.com
+ *
+ *   Copyright © 2011-2024 Belavier Commerce LLC
+ *
+ *   This source file is subject to Open Software License (OSL 3.0)
+ *   License details is bundled with this package in the file LICENSE.txt.
+ *   It is also available at this URL:
+ *   <http://www.opensource.org/licenses/OSL-3.0>
+ *
+ *  UPGRADE NOTE:
+ *    Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+ *    versions in the future. If you wish to customize AbanteCart for your
+ *    needs please refer to http://www.AbanteCart.com for more information.
+ */
 if (!defined('DIR_CORE')) {
-    exit;
+    header('Location: static_pages/');
 }
+require_once(DIR_EXT.'banner_manager'.DS.'core'.DS.'custom_block_hook.php');
+$controllers = [
+    'storefront' => [
+        'responses/extension/banner_manager',
+        'blocks/banner_block',
+    ],
+    'admin'      => [
+        'pages/extension/banner_manager',
+        'responses/listing_grid/banner_manager',
+        'pages/extension/banner_manager_stat',
+        'responses/listing_grid/banner_manager_stat',
+        'responses/extension/banner_manager_chart',
+    ],
+];
 
-class ExtensionAcwSupplierManager extends Extension
-{
-    public function onControllerPagesIndex()
-    {
-        if ($this->baseObject->user->canAccess('pages/extension/acw_supplier_manager')) {
+$models = [
+    'storefront' => ['extension/banner_manager'],
+    'admin'      => ['extension/banner_manager'],
+];
 
-            $this->baseObject->view->assign(
-                'acw_supplier_manager',
-                array(
-                    'text' => 'ACW Supplier Manager',
-                    'href' => $this->baseObject->html->getSecureURL(
-                        'extension/acw_supplier_manager'
-                    )
-                )
-            );
-        }
-    }
-}
+$languages = [
+    'storefront' => [
+        'banner_manager/banner_manager',
+    ],
+    'admin'      => [
+        'banner_manager/banner_manager',
+    ],
+];
+
+$templates = [
+    'storefront' => [
+        'blocks/banner_block.tpl',
+        'blocks/banner_block_content.tpl',
+        'blocks/banner_block_header.tpl',
+        'blocks/banner_block/one_by_one_slider_banner_block.tpl',
+        'blocks/banner_block/flex_slider_banner_block.tpl',
+
+    ],
+    'admin'      => [
+        'pages/extension/banner_manager.tpl',
+        'pages/extension/banner_manager_form.tpl',
+        'responses/extension/banner_listing.tpl',
+        'pages/extension/banner_manager_stat.tpl',
+        'pages/extension/banner_manager_stat_details.tpl',
+        'pages/extension/banner_manager_block_form.tpl',
+    ],
+];
